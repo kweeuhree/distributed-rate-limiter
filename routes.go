2812eedee8cd76630f3805f317c11c91e2c6dc46
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-25 15:21:42 -0500

    feat(routes): create alice middleware chain

--- routes.go
+++ routes.go
@@ -4,14 +4,17 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	v1 := "/api/v1"
 	router.HandlerFunc(http.MethodGet, v1+"/get", app.v1GetEndpoint)
 	router.HandlerFunc(http.MethodPost, v1+"/post", app.v1PostEndpoint)
 
-	return router
+	chain := alice.New(app.LogRequest, app.RateLimiterMiddleware)
+
+	return chain.Then(router)
 }
